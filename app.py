1e618ad643183f3759cab6442666a199c2d71e19
Luís Filipe Medeiros <lfmedeiros.rj@gmail.com>
2025-11-03 12:50:25 -0300

    templates ok

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template("_base.html")
+# Rota da página inicial
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/contacts")
+def contacts_page():
+    return render_template("contacts.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/newpad")
+def newpad_page():
+    return render_template("newpad.html")
+
+@app.route("/privacy")
+def privacy_page():
+    return render_template("privacy.html")
+
+@app.route("/search")
+def search_page():
+    return render_template("search.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBo9_Az88XTHvlCrEsiWM-aLqHRe1bSdB4",
+  authDomain: "frontenders-edb20.firebaseapp.com",
+  projectId: "frontenders-edb20",
+  storageBucket: "frontenders-edb20.firebasestorage.app",
+  messagingSenderId: "267422109015",
+  appId: "1:267422109015:web:a400232375603dfbcd0212"
+};
